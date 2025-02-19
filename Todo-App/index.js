7a8833e5a0877d25da654aea48353b06ab49d259
Bhabin <poon.bhabin6332@gmail.com>
2025-02-19 23:39:24 +0545

    add task and clear input field

--- Todo-App/index.js
+++ Todo-App/index.js
@@ -1,1 +1,23 @@
-alert("Hello world");
+let addTaskButton = document.getElementById("addTaskButton");
+
+addTaskButton.addEventListener("click", () => {
+  // Selecting task list
+  let taskList = document.getElementById("taskList");
+
+  // Creating 'li' tag/element
+  let taskItem = document.createElement("li");
+
+  // Set Class Name for each new task items
+  taskItem.className = "taskItem";
+
+  // Set input text into task item
+  let inputTask = document.getElementById("inputTask");
+  taskItem.textContent = inputTask.value;
+
+  // Clearing input field
+  inputTask.value = "";
+
+  // And at last adding task item in task list
+
+  taskList.appendChild(taskItem);
+});
