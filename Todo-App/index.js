54e7afba4c79800858727c797cce5ca89791a35a
Bhabin <poon.bhabin6332@gmail.com>
2025-02-21 16:56:47 +0545

    add feature like recent added task appear first, validate blank input task etc.

--- Todo-App/index.js
+++ Todo-App/index.js
@@ -4,24 +4,48 @@ let addTaskButton = document.getElementById("addTaskButton");
 let taskList = document.getElementById("taskList");
 
 addTaskButton.addEventListener("click", () => {
-  // Creating 'li' tag/element
-  let taskItem = document.createElement("li");
+  // Check for valid input like, blank input is not added to task list
+  if (inputTask.value.length) {
+    // Creating 'li' tag/element
+    let taskItem = document.createElement("li");
 
-  // Set Class Name for each new task items
-  taskItem.className = "taskItem";
+    // Set Class Name for each new task items
+    taskItem.className = "taskItem";
 
-  // Set input text into task item
-  let inputTask = document.getElementById("inputTask");
-  taskItem.textContent = inputTask.value;
+    // Set input text into task item
+    let inputTask = document.getElementById("inputTask");
+    taskItem.textContent = inputTask.value;
 
-  // Clearing input field
-  inputTask.value = "";
+    // Create Edit Button
+    let editButton = document.createElement("button");
+    editButton.id = "editTaskButton";
+    editButton.innerText = "Edit"; // Set Button text
+    taskItem.appendChild(editButton);
 
-  // And at last adding task item in task list
+    // Create Delete Button
+    let deleteButton = document.createElement("button");
+    deleteButton.id = "deleteTaskButton";
+    deleteButton.innerText = "Delete"; // Set Button text
+    taskItem.appendChild(deleteButton);
 
-  taskList.appendChild(taskItem);
+    deleteButton.addEventListener("click", () => {
+      taskItem.remove();
+    });
+
+    // And at last adding task item in task list
+    taskList.prepend(taskItem);
+
+    // Clearing input field
+    inputTask.value = "";
+
+    let editTaskButton = document.getElementById("editTaskButton");
+    editTaskButton.addEventListener("click", () => {
+      console.log(editTaskButton.parentElement);
+    });
+  }
 });
 
+// Clear all task
 let clearTaskButton = document.getElementById("clearTaskButton");
 
 clearTaskButton.addEventListener("click", () => {
