0c9b5cdd80b96b959061c1af73e0e8836dd6e089
Bhabin <poon.bhabin6332@gmail.com>
2025-02-22 18:11:07 +0545

    work on editing functionality of todo

--- Todo-App/index.js
+++ Todo-App/index.js
@@ -28,19 +28,70 @@ addTaskButton.addEventListener("click", () => {
     deleteButton.innerText = "Delete"; // Set Button text
     taskItem.appendChild(deleteButton);
 
+    // Clear All Task At Once
     deleteButton.addEventListener("click", () => {
       taskItem.remove();
     });
 
-    // And at last adding task item in task list
+    // Pre-Append task items
     taskList.prepend(taskItem);
 
     // Clearing input field
     inputTask.value = "";
 
+    // Edit Funcationality
     let editTaskButton = document.getElementById("editTaskButton");
     editTaskButton.addEventListener("click", () => {
-      console.log(editTaskButton.parentElement);
+      // Create input field
+      let inputField = document.createElement("input");
+      inputField.type = "text";
+      inputField.value = taskItem.childNodes[0].textContent;
+      console.log(taskItem.childNodes[0].textContent);
+
+      // Replace text with input field
+      taskItem.replaceChild(inputField, taskItem.childNodes[0]);
+
+      // Hide edit and delete button
+      editButton.style.display = "none";
+      deleteButton.style.display = "none";
+
+      // Create Update Button
+      let updateButton = document.createElement("button");
+      updateButton.textContent = "Update";
+      updateButton.id = "updateButton";
+
+      // Create Cancel Button
+      let cancelButton = document.createElement("button");
+      cancelButton.textContent = "Cancel";
+      cancelButton.id = "cancelButton";
+
+      // Add New Buttons
+      taskItem.appendChild(updateButton);
+      taskItem.appendChild(cancelButton);
+
+      // Update Functionality
+      updateButton.addEventListener("click", () => {
+        taskItem.textContent = inputField.value;
+
+        taskItem.replaceChild(taskItem.childNodes[0], inputField);
+
+        editButton.style.display = "inline-block";
+        deleteButton.style.display = "inline-block";
+
+        cancelButton.remove();
+        updateButton.remove();
+      });
+
+      // Cancle Functionality
+      cancelButton.addEventListener("click", () => {
+        taskItem.replaceChild(taskItem.childNodes[0], inputField);
+
+        editButton.style.display = "inline-block";
+        deleteButton.style.display = "inline-block";
+
+        cancelButton.remove();
+        updateButton.remove();
+      });
     });
   }
 });
