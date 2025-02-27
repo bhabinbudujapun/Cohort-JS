6aa6f117df3bb3cdff3298155211ab90ff23bc93
Bhabin <poon.bhabin6332@gmail.com>
2025-02-27 22:46:34 +0545

    update code readability

--- DOM-Challenges[1-5]/challenge-1/app.js
+++ DOM-Challenges[1-5]/challenge-1/app.js
@@ -1,19 +1,15 @@
 const toggleButton = document.getElementById("toggleButton");
+const bodyElement = document.body;
+const bulbElement = document.getElementById("bulb");
+const statusElement = document.getElementById("status");
 
 toggleButton.addEventListener("click", () => {
-  // Select 'body' id and toggle the 'dark-mode' class
-  const bodyElement = document.getElementById("body");
-  bodyElement.classList.toggle("dark-mode");
+  // Toggle dark moode
+  const isDarkMode = bodyElement.classList.toggle("dark-mode");
 
-  // Select 'bulb' id and toggle the 'off' class
-  const bulbElement = document.getElementById("bulb");
+  // Toggle bulb state
   bulbElement.classList.toggle("off");
 
-  // Select 'status' id and
-  const statusElement = document.getElementById("status");
-  if (statusElement.textContent === "Status: Off") {
-    statusElement.textContent = "Status: On";
-  } else {
-    statusElement.textContent = "Status: Off";
-  }
+  // Update status text
+  statusElement.textContent = `Status: ${isDarkMode ? "On" : "Off"}`;
 });
