e049d496faea808a5511e25507f22cca81806ffe
Bhabin <poon.bhabin6332@gmail.com>
2025-02-27 18:04:35 +0545

    Complete DOM-Challenge-1 Problem

--- DOM-Challenges[1-5]/challenge-1/app.js
+++ DOM-Challenges[1-5]/challenge-1/app.js
@@ -1,3 +1,19 @@
-/**
- * Write your challenge solution here
- */
+const toggleButton = document.getElementById("toggleButton");
+
+toggleButton.addEventListener("click", () => {
+  // Select 'body' id and toggle the 'dark-mode' class
+  const bodyElement = document.getElementById("body");
+  bodyElement.classList.toggle("dark-mode");
+
+  // Select 'bulb' id and toggle the 'off' class
+  const bulbElement = document.getElementById("bulb");
+  bulbElement.classList.toggle("off");
+
+  // Select 'status' id and
+  const statusElement = document.getElementById("status");
+  if (statusElement.textContent === "Status: Off") {
+    statusElement.textContent = "Status: On";
+  } else {
+    statusElement.textContent = "Status: Off";
+  }
+});
