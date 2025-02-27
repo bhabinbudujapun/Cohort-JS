60d7e87dca9983942bd8db3232be41cee7d6a64e
Bhabin <poon.bhabin6332@gmail.com>
2025-02-27 23:49:40 +0545

    complete DOM-Challenge-02

--- DOM-Challenges[1-5]/challenge-2/app.js
+++ DOM-Challenges[1-5]/challenge-2/app.js
@@ -1,3 +1,20 @@
-/**
- * Write your challenge solution here
- */
+// Select the parent element
+const colorButtonsDiv = document.querySelector(".color-buttons");
+
+// Select the main-heading
+const h1 = document.getElementById("mainHeading");
+
+// Add a click event listener to the parent div
+colorButtonsDiv.addEventListener("click", (event) => {
+  // Check if a button was clicked
+  if (event.target.tagName === "BUTTON") {
+    // Get the id of clicked button
+    const clickedButtonRef = document.getElementById(event.target.id);
+
+    // Extract the Style of element
+    const style = window.getComputedStyle(clickedButtonRef);
+
+    // Add a button background-color into main-heading
+    h1.style.color = style.backgroundColor;
+  }
+});
