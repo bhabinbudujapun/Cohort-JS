aeee5e010f91639f11195e691f32b719325e2c23
Bhabin <poon.bhabin6332@gmail.com>
2025-02-28 16:36:55 +0545

    select references of elements, create elements and append according to format and provide some property to its child

--- DOM-Challenges[1-5]/challenge-4/app.js
+++ DOM-Challenges[1-5]/challenge-4/app.js
@@ -1,3 +1,27 @@
-/**
- * Write your challenge solution here
- */
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
+const taskInput = document.getElementById("taskInput");
+
+addButton.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.classList.add("task-item");
+
+  const input = document.createElement("input");
+  input.classList.add("complete-checkbox");
+
+  const span = document.createElement("span");
+  span.textContent = taskInput.value;
+
+  const button = document.createElement("button");
+  button.classList.add("delete-button");
+  button.style.marginLeft = "auto";
+  button.textContent = "Delete";
+
+  li.appendChild(input);
+  li.appendChild(span);
+  li.appendChild(button);
+
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+});
