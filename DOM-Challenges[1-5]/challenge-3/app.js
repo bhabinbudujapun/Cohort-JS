46d52785dee9b8f2235a7a6952565ff1f6ab93ce
Bhabin <poon.bhabin6332@gmail.com>
2025-02-28 11:42:05 +0545

    complete dom-challenge-03

--- DOM-Challenges[1-5]/challenge-3/app.js
+++ DOM-Challenges[1-5]/challenge-3/app.js
@@ -1,3 +1,16 @@
-/**
- * Write your challenge solution here
- */
+const nameInput = document.getElementById("nameInput");
+const jobInput = document.getElementById("jobInput");
+const ageInput = document.getElementById("ageInput");
+const bioInput = document.getElementById("bioInput");
+
+const nameDisplay = document.getElementById("nameDisplay");
+const jobDisplay = document.getElementById("jobDisplay");
+const ageDisplay = document.getElementById("ageDisplay");
+const bioDisplay = document.getElementById("bioDisplay");
+
+document.addEventListener("input", () => {
+  nameDisplay.textContent = nameInput.value;
+  jobDisplay.textContent = jobInput.value;
+  ageDisplay.textContent = ageInput.value;
+  bioDisplay.textContent = bioInput.value;
+});
