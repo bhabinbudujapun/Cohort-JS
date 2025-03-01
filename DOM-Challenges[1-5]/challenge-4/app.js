b8e95059073a9b227f1e7c1ffa3f40c6f727d1d7
Bhabin <poon.bhabin6332@gmail.com>
2025-03-01 17:46:45 +0545

    add basic requirement of project

--- DOM-Challenges[1-5]/challenge-4/app.js
+++ DOM-Challenges[1-5]/challenge-4/app.js
@@ -1,16 +1,29 @@
 const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 const taskInput = document.getElementById("taskInput");
+const totalTasks = document.getElementById("totalTasks");
+const completedTasks = document.getElementById("completedTasks");
+
+function uniqueIdGenerator() {
+  return Date.now();
+}
+
+// Completed Tasks
+let completedTasksNumber = 0;
 
 addButton.addEventListener("click", () => {
   const li = document.createElement("li");
   li.classList.add("task-item");
 
   const input = document.createElement("input");
+  input.type = "checkbox";
+  input.id = uniqueIdGenerator();
+
   input.classList.add("complete-checkbox");
 
-  const span = document.createElement("span");
-  span.textContent = taskInput.value;
+  const label = document.createElement("label");
+  label.textContent = taskInput.value;
+  label.htmlFor = input.id;
 
   const button = document.createElement("button");
   button.classList.add("delete-button");
@@ -18,10 +31,31 @@ addButton.addEventListener("click", () => {
   button.textContent = "Delete";
 
   li.appendChild(input);
-  li.appendChild(span);
+  li.appendChild(label);
   li.appendChild(button);
 
-  taskList.appendChild(li);
+  // Add recent task from the top
+  taskList.prepend(li);
 
+  // Add recent task from the bottom
+  /*  taskList.appendChild(li); */
+
+  // Clear task input element
   taskInput.value = "";
+
+  // Total Task
+  let totalNumberTask = taskList.childElementCount;
+  totalTasks.textContent = `Total tasks: ${totalNumberTask}`;
+
+  // Check the checkbox of the task
+  const changeOnCheckBox = document.getElementById(input.id);
+  changeOnCheckBox.addEventListener("change", () => {
+    if (changeOnCheckBox.checked) {
+      completedTasksNumber += 1;
+      completedTasks.textContent = `Total tasks: ${completedTasksNumber}`;
+    } else {
+      completedTasksNumber -= 1;
+      completedTasks.textContent = `Total tasks: ${completedTasksNumber}`;
+    }
+  });
 });
