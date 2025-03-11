eeec87d0487271fe55d6bc7b27f2a08b742411d7
Bhabin <poon.bhabin6332@gmail.com>
2025-03-11 15:39:10 +0545

    logic portion to make accordion as intent

--- DOM-Challenges[6-10]/challenge-7/app.js
+++ DOM-Challenges[6-10]/challenge-7/app.js
@@ -1,3 +1,14 @@
-/**
- * Write your challenge solution here
- */
+// Select all the accordion buttons
+const buttonElements = document.querySelectorAll(".accordion-button");
+
+buttonElements.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Find the corresponding arrow and content for the clicked button
+    const arrowElement = this.querySelector(".arrow"); // `this` refers to the clicked button
+    const contentElement = this.nextElementSibling; // The sibling <div> with the class 'accordion-content'
+
+    // Toggle the active class for both arrow and content
+    arrowElement.classList.toggle("active");
+    contentElement.classList.toggle("active");
+  });
+});
