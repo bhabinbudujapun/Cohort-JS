ad73d502f2ede9ad46e3053892f7701cea642c06
Bhabin <poon.bhabin6332@gmail.com>
2025-03-13 23:47:15 +0545

    add product to card and check for existing product

--- DOM-Challenges[6-10]/challenge-8/app.js
+++ DOM-Challenges[6-10]/challenge-8/app.js
@@ -1,3 +1,16 @@
-/**
- * Write your challenge solution here
- */
+let cart = [];
+
+function addToCart(productName, productPrice) {
+  // Check existing product, One product at a time
+  const productExist = cart.find(
+    (product) => product.productName == productName
+  );
+
+  if (productExist) {
+    productExist.productQuantity++;
+  } else {
+    cart.push({ productName, productPrice, productQuantity: 1 }); // initial product quantity: 1
+  }
+}
+
+function subtractToCart(productIndex) {}
