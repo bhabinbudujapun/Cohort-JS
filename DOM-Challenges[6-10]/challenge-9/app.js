a6a27e4c87f96d0d32a7bd4cfef5e73431a9ec1b
Bhabin <poon.bhabin6332@gmail.com>
2025-04-10 16:29:55 +0545

    dom manipulation to make sliding menu

--- DOM-Challenges[6-10]/challenge-9/app.js
+++ DOM-Challenges[6-10]/challenge-9/app.js
@@ -1,3 +1,18 @@
-/**
- * Write your challenge solution here
- */
+const panelElement = document.querySelector(".panel");
+const closeBtnElement = document.querySelector(".close-btn");
+const toggleBtnElement = document.querySelector(".toggle-btn");
+
+toggleBtnElement.addEventListener("click", () => {
+  panelElement.classList.toggle("active");
+  if (panelElement.classList.contains("active")) {
+    panelElement.style.right = "0px";
+  } else {
+    panelElement.style.right = "-360px";
+  }
+  console.log(panelElement);
+});
+
+closeBtnElement.addEventListener("click", () => {
+  panelElement.classList.remove("active");
+  panelElement.style.right = "-360px";
+});
