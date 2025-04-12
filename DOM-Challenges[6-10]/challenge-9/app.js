5f4478f361cb75655fbb1d520acf6ec96ef93ffc
Bhabin <poon.bhabin6332@gmail.com>
2025-04-12 10:49:03 +0545

    optimize code, for menu sliding, use toogle method

--- DOM-Challenges[6-10]/challenge-9/app.js
+++ DOM-Challenges[6-10]/challenge-9/app.js
@@ -4,15 +4,8 @@ const toggleBtnElement = document.querySelector(".toggle-btn");
 
 toggleBtnElement.addEventListener("click", () => {
   panelElement.classList.toggle("active");
-  if (panelElement.classList.contains("active")) {
-    panelElement.style.right = "0px";
-  } else {
-    panelElement.style.right = "-360px";
-  }
-  console.log(panelElement);
 });
 
 closeBtnElement.addEventListener("click", () => {
   panelElement.classList.remove("active");
-  panelElement.style.right = "-360px";
 });
