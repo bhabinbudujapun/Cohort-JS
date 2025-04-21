51b27190d2a7b3abf0d584776b9933ec14e0084e
Bhabin <poon.bhabin6332@gmail.com>
2025-04-21 09:26:39 +0545

    Complete update, edit, delete section | Complete Project

--- DOM-Challenges[1-5]/challenge-4/app.js
+++ DOM-Challenges[1-5]/challenge-4/app.js
@@ -11,6 +11,9 @@ function uniqueIdGenerator() {
 // Completed Tasks
 let completedTasksNumber = 0;
 
+// Total number of Tasks
+let totalNumberTask = 0;
+
 addButton.addEventListener("click", () => {
   // Check for empty and whitespace value
   if (!taskInput.value.trim().length) return;
@@ -34,15 +37,15 @@ addButton.addEventListener("click", () => {
   divInputTask.appendChild(input);
   divInputTask.appendChild(label);
 
-  const deleteButton = document.createElement("button");
-  deleteButton.type = "button";
-  deleteButton.classList.add("delete-button");
-  deleteButton.textContent = "Delete";
-
   const editButton = document.createElement("button");
   editButton.type = "button";
   editButton.classList.add("edit-button");
   editButton.textContent = "Edit";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.type = "button";
+  deleteButton.classList.add("delete-button");
+  deleteButton.textContent = "Delete";
 
   // Create div element for button-task
   const divButtonTask = document.createElement("div");
@@ -56,38 +59,94 @@ addButton.addEventListener("click", () => {
   // Add recent task from the top
   taskList.prepend(li);
 
+  // Functionality edit task
+  editButton.addEventListener("click", () => {
+    // Create input element with type text and replace label
+    const inputFiled = document.createElement("input");
+    inputFiled.type = "text";
+    inputFiled.value = label.textContent;
+    divInputTask.replaceChild(inputFiled, label);
+
+    // Create Update button
+    const updateButton = document.createElement("button");
+    updateButton.type = "button";
+    updateButton.classList.add("edit-button");
+    updateButton.textContent = "Update";
+
+    // Create Cancel button
+    const cancelButton = document.createElement("button");
+    cancelButton.type = "button";
+    cancelButton.classList.add("delete-button");
+    cancelButton.textContent = "Cancel";
+
+    // Hide Edit and Delete buttons
+    editButton.style.display = "none";
+    deleteButton.style.display = "none";
+
+    divButtonTask.appendChild(updateButton);
+    divButtonTask.appendChild(cancelButton);
+
+    // Update Functionality
+    updateButton.addEventListener("click", () => {
+      // replace input lable into input filed
+      label.textContent = inputFiled.value;
+      divInputTask.replaceChild(label, inputFiled);
+
+      // display edit and delete buttons
+      editButton.style.display = "inline-block";
+      deleteButton.style.display = "inline-block";
+
+      // remove update and cancel button
+      updateButton.remove();
+      cancelButton.remove();
+    });
+
+    // Cancel Functionality
+    cancelButton.addEventListener("click", () => {
+      // replace input field into label
+      divInputTask.replaceChild(label, inputFiled);
+
+      // display edit and delete buttons
+      editButton.style.display = "inline-block";
+      deleteButton.style.display = "inline-block";
+
+      // remove update and cancel buttons
+      updateButton.remove();
+      cancelButton.remove();
+    });
+  });
+
+  // Total Task
+  totalNumberTask = taskList.childElementCount;
+  totalTasks.textContent = `Total tasks: ${totalNumberTask}`;
+
+  // Functionality delete task
+  deleteButton.addEventListener("click", () => {
+    if (changeOnCheckBox.checked) {
+      completedTasksNumber -= 1;
+      completedTasks.textContent = `Completed: ${completedTasksNumber}`;
+    }
+    const liElement = deleteButton.parentElement.parentElement;
+    totalNumberTask -= 1;
+    totalTasks.textContent = `Total tasks: ${totalNumberTask}`;
+    liElement.remove();
+  });
+
+  // ** UNCOMMENT ACCORDING TO YOUR REQUIREMENT ***
   // Add recent task from the bottom
   /*  taskList.appendChild(li); */
 
   // Clear task input element
   taskInput.value = "";
 
-  // Total Task
-  let totalNumberTask = taskList.childElementCount;
-  totalTasks.textContent = `Total tasks: ${totalNumberTask}`;
-
-  // Delete functionality
-  deleteButton.addEventListener("click", () => {
-    deleteButton.parentElement.parentElement.remove();
-    totalNumberTask -= 1;
-    
-    totalTasks.textContent = `Total tasks: ${totalNumberTask}`;
-  });
-
-  // Edit functionality
-  editButton.addEventListener("click", () => {
-    console.log("edit");
-  });
-
   // Check the checkbox of the task
   const changeOnCheckBox = document.getElementById(input.id);
   changeOnCheckBox.addEventListener("change", () => {
     if (changeOnCheckBox.checked) {
       completedTasksNumber += 1;
-      completedTasks.textContent = `Completed: ${completedTasksNumber}`;
     } else {
       completedTasksNumber -= 1;
-      completedTasks.textContent = `Completed: ${completedTasksNumber}`;
     }
+    completedTasks.textContent = `Completed: ${completedTasksNumber}`;
   });
 });
